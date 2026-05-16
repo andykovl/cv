efaed5040be63ea79d411ddd822d11ee1b524907
Andrey Kovlyagin <andrey.kovlyagin@gmail.com>
2026-05-17 00:04:05 +0200

    vite updated for new url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/cv-wikipedia-pdf/",
+	base: "/cv/",
 	plugins: [tailwindcss()],
 });
